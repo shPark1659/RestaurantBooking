c3ded42005eaa64afc1ad64cc3a4651ec70f567c
shpark <sh0913.park@samsung.com>
2024-06-14 11:56:48 +0900

    feat: add test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공

--- test_booking_scheduler.py
+++ test_booking_scheduler.py
@@ -7,16 +7,20 @@ from schedule import Customer, Schedule
 
 NOT_ON_TIME_TIMESTAMP = datetime.strptime("2021/03/26 09:05", "%Y/%m/%d %H:%M")
 ON_TIME_TIMESTAMP = datetime.strptime("2021/03/26 09:00", "%Y/%m/%d %H:%M")
+DIFFERENT_TIME_TIMESTAMP = datetime.strptime("2021/03/26 10:00", "%Y/%m/%d %H:%M")
 CUSTOMER = Customer("Fake name", "010-1234-5678")
+CAPACITY_PER_HOUR = 3
+UNDER_CAPACITY = 1
+OVER_CAPACITY = 4
 
 
 class BookingSchedulerTest(unittest.TestCase):
     def setUp(self):
-        self.booking_scheduler = BookingScheduler(3)
+        self.booking_scheduler = BookingScheduler(CAPACITY_PER_HOUR)
 
     def test_예약은_정시에만_가능하다_정시가_아닌경우_예약불가(self):
         # arrange
-        schedule = Schedule(NOT_ON_TIME_TIMESTAMP, 1, CUSTOMER)
+        schedule = Schedule(NOT_ON_TIME_TIMESTAMP, UNDER_CAPACITY, CUSTOMER)
 
         # act and assert
         with self.assertRaises(ValueError):
@@ -24,7 +28,7 @@ class BookingSchedulerTest(unittest.TestCase):
 
     def test_예약은_정시에만_가능하다_정시인_경우_예약가능(self):
         # arrange
-        schedule = Schedule(ON_TIME_TIMESTAMP, 1, CUSTOMER)
+        schedule = Schedule(ON_TIME_TIMESTAMP, UNDER_CAPACITY, CUSTOMER)
 
         # act
         self.booking_scheduler.add_schedule(schedule)
@@ -34,7 +38,7 @@ class BookingSchedulerTest(unittest.TestCase):
 
     def test_시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생(self):
         # arrange
-        schedule = Schedule(ON_TIME_TIMESTAMP, 4, CUSTOMER)
+        schedule = Schedule(ON_TIME_TIMESTAMP, OVER_CAPACITY, CUSTOMER)
 
         # act and assert
         with self.assertRaises(ValueError):
@@ -42,7 +46,17 @@ class BookingSchedulerTest(unittest.TestCase):
 
     @skip
     def test_시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공(self):
-        pass
+        # arrange
+        schedule1 = Schedule(ON_TIME_TIMESTAMP, CAPACITY_PER_HOUR, CUSTOMER)
+        schedule2 = Schedule(DIFFERENT_TIME_TIMESTAMP, CAPACITY_PER_HOUR, CUSTOMER)
+
+        # act
+        self.booking_scheduler.add_schedule(schedule1)
+        self.booking_scheduler.add_schedule(schedule2)
+
+        # assert
+        self.assertTrue(self.booking_scheduler.has_schedule(schedule1))
+        self.assertTrue(self.booking_scheduler.has_schedule(schedule2))
 
     @skip
     def test_예약완료시_SMS는_무조건_발송(self):
